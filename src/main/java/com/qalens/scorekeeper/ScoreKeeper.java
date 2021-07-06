763c513cfe366e3fe527a261667a8e9f608f4423
Naik <atm@Naiks-MacBook-Air.local>
2021-07-06 09:04:20 +0530

    ATM | pass test for team b 1

--- src/main/java/com/qalens/scorekeeper/ScoreKeeper.java
+++ src/main/java/com/qalens/scorekeeper/ScoreKeeper.java
@@ -4,9 +4,10 @@ public class ScoreKeeper {
     private int teamAScore=0;
     private int teamBScore=0;
     public String getScore(){
-        return String.format("%03d:%03d",teamAScore,teamAScore);
+        return String.format("%03d:%03d",teamAScore,teamBScore);
     }
 
     public void scoreTeamB1() {
+        teamBScore++;
     }
 }
